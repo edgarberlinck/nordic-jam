397cc713df97f6dc1642c6bda64a44192a57ac12
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2025-02-18 11:01:47 +0100

    feat: adding ProfileInfluences

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,8 @@ class Profile < ApplicationRecord
   has_many :instruments, through: :profile_instruments
   has_many :profile_styles
   has_many :styles, through: :profile_styles
+  has_many :influences
+  has_many :influenced_by, through: :influences
 
   validates :full_name, presence: true
   validates :address, presence: true
