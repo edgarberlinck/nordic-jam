2b96dd86d24830c3144d7ec4a9c3a3b4190f3d2c
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2025-02-18 10:30:19 +0100

    feat: adding ProfileStyles model

---
+++ app/models/profile_style.rb
@@ -0,0 +1,4 @@
+class ProfileStyle < ApplicationRecord
+  belongs_to :profile
+  belongs_to :style
+end
---
+++ test/models/profile_style_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProfileStyleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
