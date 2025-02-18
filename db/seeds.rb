01ccaba36f1355fdf7dbef0855f6634d0ac58d38
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2025-02-18 11:05:12 +0100

    data: adding seed to create the Influences

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,3 +53,43 @@ end
 ].each do |style|
   Style.find_or_create_by(name: style)
 end
+
+[
+  # Classic Rock
+  "The Beatles", "The Rolling Stones", "Led Zeppelin", "Pink Floyd", "Queen",
+  "The Doors", "The Who", "Fleetwood Mac", "Jimi Hendrix", "Creedence Clearwater Revival",
+
+  # Metal & Hard Rock
+  "Black Sabbath", "Metallica", "Iron Maiden", "Judas Priest", "Slayer",
+  "Megadeth", "Pantera", "Deep Purple", "Motörhead", "Tool",
+
+  # Alternative & Grunge
+  "Nirvana", "Pearl Jam", "Soundgarden", "Alice in Chains", "Smashing Pumpkins",
+  "Red Hot Chili Peppers", "Foo Fighters", "Radiohead", "Beck", "Nine Inch Nails",
+
+  # Punk & Post-Punk
+  "The Ramones", "The Clash", "Sex Pistols", "Dead Kennedys", "Misfits",
+  "Joy Division", "The Cure", "Siouxsie and the Banshees", "Talking Heads", "The Smiths",
+
+  # Pop & New Wave
+  "David Bowie", "Prince", "Madonna", "Michael Jackson", "Elton John",
+  "Depeche Mode", "Tears for Fears", "Duran Duran", "Pet Shop Boys", "Eurythmics",
+
+  # Hip-Hop & Rap
+  "Tupac Shakur", "The Notorious B.I.G.", "N.W.A", "Run-D.M.C.", "Public Enemy",
+  "Wu-Tang Clan", "OutKast", "Kanye West", "Jay-Z", "Eminem",
+
+  # Electronic & Dance
+  "Daft Punk", "Kraftwerk", "The Prodigy", "Aphex Twin", "Moby",
+  "Underworld", "Fatboy Slim", "Justice", "Deadmau5", "The Chemical Brothers",
+
+  # Jazz & Blues
+  "Miles Davis", "John Coltrane", "Louis Armstrong", "B.B. King", "Muddy Waters",
+  "Charlie Parker", "Duke Ellington", "Thelonious Monk", "Ella Fitzgerald", "Ray Charles",
+
+  # Folk & Country
+  "Bob Dylan", "Johnny Cash", "Willie Nelson", "Neil Young", "Joan Baez",
+  "Joni Mitchell", "Patsy Cline", "Dolly Parton", "Hank Williams", "Simon & Garfunkel"
+].each do |influence|
+  Influence.find_or_create_by(name: influence)
+end
