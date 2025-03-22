8fa31937d8e69cae985513b8a92257e4b2a6faa5
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2025-03-23 00:26:58 +0100

    build: remove unused field

--- db/migrate/20250322200910_create_profile_created_bands.rb
+++ db/migrate/20250322200910_create_profile_created_bands.rb
@@ -3,7 +3,6 @@ class CreateProfileCreatedBands < ActiveRecord::Migration[8.0]
     create_table :profile_created_bands do |t|
       t.references :profile, null: false, foreign_key: true
       t.string :title
-      t.string :description
 
       t.timestamps
     end
